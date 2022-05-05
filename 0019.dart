2ffab8c6effc290e2ead038750c9663167c2a4ab
Baxtiyor <baxtiyor98@yandex.ru>
2022-05-05 11:03:58 +0300

    0019 is added

--- 0019.dart
+++ 0019.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
+
 main() async {
   Iterable<int> N = stdin.readLineSync()!.split(" ").map((e) => int.parse(e));
   int one = N.elementAt(0);
   int two = N.elementAt(1);
-  
-  
+  print(two ~/ one);
 }
