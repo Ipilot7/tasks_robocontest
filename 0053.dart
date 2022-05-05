9c6c4dfb972fec888b4dac6fd48d4b3bfcf16ba9
Baxtiyor <baxtiyor98@yandex.ru>
2022-05-05 18:28:42 +0300

    0053 is modified

--- 0053.dart
+++ 0053.dart
@@ -4,5 +4,9 @@ import 'dart:math';
 
 void main(List<String> args) {
   double n = double.parse(stdin.readLineSync()!);
-  print(((pow(n, 2) - 3 * n) / 2).toInt());
+  if (n > 2) {
+    print(((pow(n, 2) - 3 * n) / 2).toInt());
+  } else {
+    print(0);
+  }
 }
