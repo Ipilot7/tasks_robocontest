f824acf04131a0bfdde7bf1849b4d8e13fd9dee2
Baxtiyor <botiraliyevbaxtiyorbaxodirov@gmail.com>
2022-05-12 09:29:45 +0300

    0021 is added

--- 0021.dart
+++ 0021.dart
@@ -2,11 +2,7 @@ import 'dart:io';
 
 void main(List<String> args) {
   List son = stdin.readLineSync()!.split(" ").map((e) => int.parse(e)).toList();
-  int sum = son.elementAt(0) + son.elementAt(1) + son.elementAt(2);
-  if (sum % 2 == 0) {
-    print(sum / 2);
-  } else {
-    print((sum / 2).floor().toInt());
-  }
+  var sum = (son.elementAt(0)/2).ceil() + (son.elementAt(1)/2).ceil() + (son.elementAt(2)/2).ceil();
+  print(sum);
   exit(0);
 }
